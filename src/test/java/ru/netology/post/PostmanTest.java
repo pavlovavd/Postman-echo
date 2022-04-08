8dd39f27944c700d0147f98d184e3af856df9d58
Victoria Pavlova <pvd0510@mail.ru>
2022-04-08 18:50:04 +0300

    Initial commit

---
+++ src/test/java/ru/netology/post/PostmanTest.java
@@ -0,0 +1,23 @@
+package ru.netology.post;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanTest {
+    @Test
+    void test1 () {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some value"))
+        ;
+    }
+}
